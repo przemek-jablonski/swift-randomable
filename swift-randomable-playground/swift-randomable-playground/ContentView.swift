ec23f88b1620c16e9145d0a3f70bca3c2b19c7ed
przemek-jablonski <przemek.jablonski@protonmail.com>
2023-08-05 14:44:32 +0200

    Empty swift-randomable-playground project

---
+++ swift-randomable-playground/swift-randomable-playground/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  swift-randomable-playground
+//
+//  Created by Przemyslaw Jablonski on 05/08/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swift-randomable-playground/swift-randomable-playground/swift_randomable_playgroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_randomable_playgroundApp.swift
+//  swift-randomable-playground
+//
+//  Created by Przemyslaw Jablonski on 05/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct swift_randomable_playgroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
