6ac10601e4b9db63a4e3c1638293af6777186822
przemek-jablonski <przemek.jablonski@protonmail.com>
2023-08-09 18:16:32 +0200

    Test cases for Optionals, MapKit, Foundation types

---
+++ swift-randomable/Tests/Randomable-Tests/Internals/XCTestCase+generateAndCompareInstances.swift
@@ -0,0 +1,30 @@
+import Randomable
+import XCTest
+
+internal extension XCTestCase {
+  func generateAndCompareInstances<T: Randomable & Equatable>(
+    of type: T.Type,
+    using randomNumberGenerator: inout any RandomNumberGenerator,
+    generation: (_ type: T.Type, _ generator: inout any RandomNumberGenerator) -> T = { $0.random(&$1) },
+    assertions: (_ instance1: T, _ instance2: T) -> () = { XCTAssertEqual($0, $1) }
+  ) {
+    generateInstances(
+      of: type,
+      using: &randomNumberGenerator,
+      generation: generation,
+      assertions: assertions
+    )
+  }
+
+  func generateInstances<T: Randomable>(
+    of type: T.Type,
+    using randomNumberGenerator: inout any RandomNumberGenerator,
+    generation: (_ type: T.Type, _ generator: inout any RandomNumberGenerator) -> T = { $0.random(&$1) },
+    assertions: (_ instance1: T, _ instance2: T) -> ()
+  ) {
+    assertions(
+      generation(type, &randomNumberGenerator),
+      generation(type, &randomNumberGenerator)
+    )
+  }
+}
---
+++ swift-randomable/Tests/Randomable-Tests/Tests/Conformances/MapKitRandomableTestCase.swift
@@ -0,0 +1,79 @@
+import MapKit
+import Randomable
+import XCTest
+
+internal class MapKitRandomableTestCase: XCTestCase {
+  var randomNumberGenerator: (any RandomNumberGenerator)!
+
+  override func setUp() {
+    self.randomNumberGenerator = StaticRandomNumberGenerator()
+  }
+
+  override func tearDown() async throws {
+    self.randomNumberGenerator = nil
+  }
+
+  internal func test_twoMKCoordinateRegionInstances_generatedUsingStaticGenerator_shouldBeEqual() {
+    generateInstances(
+      of: MKCoordinateRegion.self,
+      using: &randomNumberGenerator,
+      assertions: { instance1, instance2 in
+        XCTAssertEqual(
+          instance1.center.latitude,
+          instance2.center.latitude
+        )
+
+        XCTAssertEqual(
+          instance1.center.longitude,
+          instance2.center.longitude
+        )
+
+        XCTAssertEqual(
+          instance1.span.longitudeDelta, 
+          instance2.span.longitudeDelta
+        )
+
+        XCTAssertEqual(
+          instance1.span.latitudeDelta,
+          instance2.span.latitudeDelta
+        )
+      }
+    )
+  }
+
+  internal func test_twoCLLocationCoordinate2DInstances_generatedUsingStaticGenerator_shouldBeEqual() {
+    generateInstances(
+      of: CLLocationCoordinate2D.self,
+      using: &randomNumberGenerator,
+      assertions: { instance1, instance2 in
+        XCTAssertEqual(
+          instance1.latitude,
+          instance2.latitude
+        )
+
+        XCTAssertEqual(
+          instance1.longitude,
+          instance2.longitude
+        )
+      }
+    )
+  }
+
+  internal func test_twoMKCoordinateSpanInstances_generatedUsingStaticGenerator_shouldBeEqual() {
+    generateInstances(
+      of: MKCoordinateSpan.self,
+      using: &randomNumberGenerator,
+      assertions: { instance1, instance2 in
+        XCTAssertEqual(
+          instance1.longitudeDelta,
+          instance2.longitudeDelta
+        )
+
+        XCTAssertEqual(
+          instance1.latitudeDelta,
+          instance2.latitudeDelta
+        )
+      }
+    )
+  }
+}
